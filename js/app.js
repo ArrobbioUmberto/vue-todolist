adda5b7391fb4629841d85d5f2b536f1a7c00756
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-21 15:33:31 +0100

    FEAT create array of objects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
 console.log('ciao')
+
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+            tasks:[ 
+                {
+                text:'Fare la spesa',
+                done: true,
+            },
+            {
+                text: 'Pulire i pavimenti',
+                done: true,
+            },
+            {
+                text: 'Lavare i piatti',
+                done: true,
+            },
+            {
+                text: 'Pagare assicurazione',
+                done: false,
+            },
+            {
+                text: 'Leggere le mail',
+                done: false,
+            },
+            {
+                text: 'Andare dal parrucchiere',
+                done: false,
+            },
+            {
+                text: 'Aggiornare PC',
+                done: false,
+            },
+
+        ]
+           }
+  }
+}).mount('#app')
